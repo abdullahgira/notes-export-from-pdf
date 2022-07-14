71aac684f52f25c088477db4c2deecb70ea81c05
Abdullah Gira <abdullaho.gira@gmail.com>
2022-07-14 18:55:01 +0200

    Filter highlight annotations before sorting them

--- app/extractor.py
+++ app/extractor.py
@@ -13,24 +13,23 @@ def extract_notes(file_name):
 
     # loop till we have highlight annotation in the page
     for page in doc:
-        page_annots = list(page.annots())
+        page_annots = [a for a in list(page.annots()) if a.type[0] == 8]
 
         # sort page annots based on their location in the page
         # by default it is sorted based on create time
-        page_annots.sort(key=lambda a: a.vertices[0][1])
+        page_annots.sort(key=lambda a: a and a.vertices[0][1])
 
         for annot in page_annots: 
-            if annot.type[0] == 8:
-                all_coordinates = annot.vertices
+            all_coordinates = annot.vertices
 
-                if len(all_coordinates) == 4:
-                    highlight_coord = fitz.Quad(all_coordinates).rect
-                    highlights.append(highlight_coord)
-                else:
-                    all_coordinates = [all_coordinates[x:x+4] for x in range(0, len(all_coordinates), 4)]
-                    for i in range(0,len(all_coordinates)):
-                        coord = fitz.Quad(all_coordinates[i]).rect
-                        highlights.append(coord)
+            if len(all_coordinates) == 4:
+                highlight_coord = fitz.Quad(all_coordinates).rect
+                highlights.append(highlight_coord)
+            else:
+                all_coordinates = [all_coordinates[x:x+4] for x in range(0, len(all_coordinates), 4)]
+                for i in range(0,len(all_coordinates)):
+                    coord = fitz.Quad(all_coordinates[i]).rect
+                    highlights.append(coord)
 
         all_words = page.get_text_words()
         for h in highlights:
