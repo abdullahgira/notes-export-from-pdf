98dd8b46907a1870c801638421ee89fb329529f0
Abdullah Gira <abdullaho.gira@gmail.com>
2022-11-19 19:32:43 +0200

    Use pdfannots to extract notes in joson format

---
+++ app/pdfannots/printer.py
@@ -0,0 +1,29 @@
+import abc
+import typing as typ
+
+
+class Printer(abc.ABC):
+    """
+    Base class for pretty-printers.
+    """
+
+    def begin(self) -> str:
+        """Called once prior to print_file call. Returns initial output."""
+        return ''
+
+    @abc.abstractmethod
+    def print_file(
+        self,
+        filename: str,
+        document
+    ) -> typ.Iterator[str]:
+        """
+        Pretty-print a single document.
+
+        Pretty-print the extracted annotations, yielding output (incrementally) as strings.
+        Called multiple times, once per file.
+        """
+
+    def end(self) -> str:
+        """Called once after the final print_file call. Returns any final additional output."""
+        return ''
